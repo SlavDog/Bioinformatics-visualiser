4d53d553af3606769265fa930f6c132baaf6b0eb
Leoš Hampl <xhampl2@fi.muni.cz>
2025-08-25 21:50:29 +0200

    feat: add graph edges generation functionality

--- src/Visualisation.jsx
+++ src/Visualisation.jsx
@@ -1,51 +1,113 @@
 import subjectOrderData from './order.json';
 import subjectInfoData from './final_tree.json'
 import Subject from './Subject';
+import {addHelperNodesAndOffsets} from './utils/helperFunctions'
 
-function Visualisation() {
-    const columnWidth = 400;
-    const rowHeight = 250;
-    const padding = 100;
-    let maxX = 0;
-    let maxY = 0;
-    
-    const positions = {}
-    Object.entries(subjectOrderData).forEach(([semester, courses]) => {
-        courses.forEach((course, i) => {
-            const x = padding + parseInt(semester - 1) * columnWidth;
-            const y = padding + i * rowHeight;
-            positions[course] = { x, y}
-            if (x + columnWidth > maxX) {maxX = x + columnWidth}
-            if (y + rowHeight > maxY) {maxY = y + rowHeight}
+const columnWidth = 400;
+const rowHeight = 175;
+const subjectHeight = 125;
+const subjectWidth = 250;
+const padding = 25;
+let maxX = 0;
+let maxY = 0;
+const colors = ["red", "blue", "green", "orange", "purple", "brown", "black", "gray", "magenta"];
+const edgeYOffsets = {};
 
+
+const newSubjectInfoData = addHelperNodesAndOffsets(subjectInfoData, subjectOrderData, edgeYOffsets);
+const yValues = {}
+Object.keys(subjectOrderData).forEach((semester) =>
+    Object.values(subjectOrderData[semester]).forEach((code, i) => yValues[code] = i)
+);
+
+const ITERATIONS = 5;
+for (let i = 0; i < ITERATIONS; i++) {
+    Object.entries(newSubjectInfoData).forEach(([code, course]) => {
+        if (course.successors.length == 0) return;
+        let sum = 0;
+        let count = 0;
+        course.successors.forEach((succ) => {
+            if (yValues[succ] !== undefined) {
+                sum += yValues[succ];
+                count++;
+            }
         });
+        console.log(code, sum, count);
+        console.log(yValues[code]);
+        yValues[code] = sum / count;
+        console.log(yValues[code]);
+        console.log("--------");
     });
+}
+
+Object.values(subjectOrderData).forEach((semester) => {
+    semester.sort((a, b) => yValues[a] - yValues[b]);
+})
+console.log(subjectOrderData);
+
+const positions = {}
+Object.entries(subjectOrderData).forEach(([semester, courses]) => {
+    courses.forEach((course, i) => {
+        const x = padding + parseInt(semester - 1) * columnWidth;
+        const y = padding + i * rowHeight;
+        positions[course] = { x, y }
+
+        if (x + columnWidth > maxX) {maxX = x + columnWidth}
+        if (y + rowHeight > maxY) {maxY = y + rowHeight}
+    });
+});
+    
+const Visualisation = () => {
     return (
         <div className="visualisationBox" 
                 style = {{
-                    width: maxX,
-                    height: maxY 
+                    width: maxX - 75,
+                    height: maxY
                 }}
         >
-            {Object.entries(subjectInfoData).map(([code, course]) => {
+            <svg className='connections'>
+                {Object.entries(newSubjectInfoData).map(([startCode, course]) => {
+                    return course.successors.map((endCode, i) => {
+
+                        const start = positions[startCode];
+                        const end = positions[endCode];
+                        if (!start || !end) { return null; }
+                        const startX = start.x + subjectWidth / 2;
+                        const startY = start.y + subjectHeight / 2;
+                        const endX = end.x + subjectWidth / 2;
+                        const endY = end.y + subjectHeight / 2;
+                        const midX = (startX + endX) / 2;
+
+                        let offset = edgeYOffsets[`${startCode}-${endCode}`]
+
+                        const path = `
+                            M ${startX} ${startY + offset}
+                            L ${midX + offset} ${startY + offset}
+                            L ${midX + offset} ${endY + offset}
+                            L ${endX} ${endY + offset}
+                        `;
+                        return (<path key={`${startCode}-${endCode}-${i}`} d={path}
+                            stroke="black" fill="transparent" strokeWidth="2" />);
+                    })}
+                )}
+            </svg>
+            {Object.entries(newSubjectInfoData).map(([code, course]) => {
                 const pos = positions[code];
-                if (!pos) return null;
+                if (!pos || course.name == "") return null;
                 return (<Subject
                     key={code}
                     code={code}
-                    name={course.name}
-                    faculty={course.faculty}
-                    language={course.language}
-                    completion={course.completion}
-                    credits={course.credits}
+                    course={course}
                     style={{
                         position: "absolute",
                         left: positions[code].x,
-                        top: positions[code].y
+                        top: positions[code].y,
+                        width: {subjectWidth},
+                        height: {subjectHeight}
                     }}
                 />)
             })}
-        </div>
+        </div> 
     );
 }
 
---
+++ src/utils/helperFunctions.jsx
@@ -0,0 +1,70 @@
+const helperNode = {
+  name: "",
+  faculty: "",
+  successors: [],
+  language: "",
+  completion: "",
+  has_successors: true,
+  has_parent: true,
+  credits: "",
+  link: ""
+};
+
+
+function parseSemester(val) {
+  return val === "null" ? null : Number(val);
+}
+
+
+function ensureOffset(allOffsets, key, offsetToAdd) {
+    if (!allOffsets[key]) {
+        allOffsets[key] = offsetToAdd; 
+    }
+    return allOffsets[key];
+}
+
+
+function createHelperNode(infoData, orderData, prevNodeCode, currentNodeCode, semester) {
+    if (!infoData[currentNodeCode]) {
+        infoData[currentNodeCode] = {
+            ...helperNode,
+            semester: semester,
+            successors: []
+        };
+        orderData[semester].push(currentNodeCode);
+    }
+    infoData[prevNodeCode].successors.push(currentNodeCode);
+}
+
+
+export function addHelperNodesAndOffsets(originalInfoData, orderData, edgeYOffsets) {
+    const newSubjectInfoData = structuredClone(originalInfoData)
+    Object.entries(originalInfoData).forEach(([parentCode, course]) => {
+        const newSuccessors = [...course.successors];
+        newSuccessors.forEach((successorCode, i) => {
+            if (!originalInfoData[successorCode]) {return;}
+            let succSemester = parseSemester(originalInfoData[successorCode].semester);
+            let parentSemester = parseSemester(course.semester);
+
+            const offset = ensureOffset(edgeYOffsets, `${parentCode}-${successorCode}`,
+                        (i - (newSuccessors.length - 1) / 2) * 12);
+
+            // Create helper nodes in layers between connected subjects from distant semesters
+            if (parentSemester != null && succSemester != null && parentSemester + 1 != succSemester) {
+                newSubjectInfoData[parentCode].successors = newSubjectInfoData[parentCode].successors
+                    .filter(item => item !== successorCode);
+                let prevNode = parentCode;
+                
+                for (let i = parentSemester + 1; i < succSemester; i++) {
+                    let helperNodeCode = `HELPER_${successorCode}_${i}`;
+                    createHelperNode(newSubjectInfoData, orderData, prevNode, helperNodeCode, i);
+                    ensureOffset(edgeYOffsets, `${prevNode}-${helperNodeCode}`, offset);
+                    prevNode = helperNodeCode;
+                }
+                newSubjectInfoData[prevNode].successors.push(successorCode);
+                ensureOffset(edgeYOffsets, `${prevNode}-${successorCode}`, offset);
+            }
+        })
+    });
+    return newSubjectInfoData;
+}
