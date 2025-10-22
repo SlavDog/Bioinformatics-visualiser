2103d64ffdd1a301f96fe389745a8d9ec8dfa27e
Leoš Hampl <xhampl2@fi.muni.cz>
2025-10-22 11:17:54 +0200

    feat: add successor lines to SubjectDetailMenu

--- src/components/SubjectDetailMenu.jsx
+++ src/components/SubjectDetailMenu.jsx
@@ -9,7 +9,7 @@ const subjectHeight = 140;
 const subjectWidth = 250;
 const subjectPadding = 16;
 
-const SubjectDetailMenu = ({open, onClose, source, setIsOpen}) => {
+const SubjectDetailMenu = ({open, onClose, source}) => {
     const [isClosing, setIsClosing] = useState(false);
     const [isVisible, setIsVisible] = useState(false);
     const [isAnimatingOpen, setIsAnimatingOpen] = useState(false);
@@ -39,17 +39,53 @@ const SubjectDetailMenu = ({open, onClose, source, setIsOpen}) => {
             <div className="menuBoxSubjectsContainer">
                 {Object.values(subjectInfoData["choices"][source]["list"]).map((code) => {
                     if (source == "tv" || source == "CORE") return "TV or CORE";
-                    return (<Subject
-                        key={code}
-                        code={code}
-                        course={subjectInfoData["details"][code]}
-                        style={{
-                            width: subjectWidth,
-                            height: subjectHeight,
-                            padding: subjectPadding,
-                            transform: "scale(0.9)"
-                        }}
-                    />)
+                    let course = subjectInfoData["details"][code];
+                    let predLineColor = course.has_predecessors ? "black" : "transparent";
+                    let succLineColor = course.has_successors ? "black" : "transparent";
+
+                    return (
+                        <div style={{display: "flex", flexDirection: "column", alignItems:"center"}}>
+                            <svg width={subjectWidth} height="60">
+                                <line 
+                                    key="vertline" 
+                                    x1={subjectWidth / 2} y1="10" 
+                                    x2={subjectWidth / 2} y2="60" 
+                                    stroke={predLineColor} 
+                                    strokeDasharray="5 5" 
+                                    strokeWidth="2" />
+                            </svg>
+                                <Subject
+                                key={code}
+                                code={code}
+                                course={course}
+                                style={{
+                                    width: subjectWidth,
+                                    height: subjectHeight,
+                                    padding: subjectPadding,
+                                    transform: "scale(0.9)"
+                                }}
+                            />
+                            <svg width={subjectWidth} height="60">
+                                <line 
+                                    key="vertline" 
+                                    x1={subjectWidth / 2} y1="0"
+                                    x2={subjectWidth / 2} y2="40" 
+                                    stroke={succLineColor}
+                                    strokeDasharray="5 5"
+                                    strokeWidth="2" />
+                                <text 
+                                    x={subjectWidth / 2} 
+                                    y="50" 
+                                    textAnchor="middle" 
+                                    fontSize="12"
+                                    fontFamily='Inter'
+                                    fill={succLineColor}
+                                >
+                                    ⬇️ Navazující: {course.successors.join(', ')}
+                                </text>
+                            </svg>
+                        </div>
+                    )
                 })}
             </div>
         </div>
