de11121482c20ce41bdb967e599a1be3ec8e5469
Leoš Hampl <llleosek@seznam.cz>
2026-03-14 13:16:09 +0100

    feat: add help icon for scrolling

--- src/components/SideBar/RangeScaler.tsx
+++ src/components/SideBar/RangeScaler.tsx
@@ -11,6 +11,9 @@ function RangeScaler({scale, setScale} : RangeScalerProps) {
     return (
         <div className='rangeScaler'>
             <label>Velikost</label>
+            <div className="helpIcon" style={{ width: "12px", height: "12px", marginLeft: "2px", fontSize: "10px" }} title="Velikost lze měnit také pomocí Ctrl + scroll">
+                ?
+            </div>
             <input type="range" id="volume" min="0.3" max="1.5" step="0.1" value={scale} onChange={(e) => setScale(Number(e.target.value))} />
         </div>
     );
