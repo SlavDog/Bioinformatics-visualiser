957a5136aa8f91ea722e84ec32c5e57eb7f9b71b
Leoš Hampl <llleosek@seznam.cz>
2026-04-18 14:14:35 +0200

    refactor: add tippy for warnings too and prettify

--- src/components/Subject/Warning.tsx
+++ src/components/Subject/Warning.tsx
@@ -1,23 +1,36 @@
-import { Course } from "@/types/subjects";
+import { Course } from '@/types/subjects';
+import Tippy from '@tippyjs/react';
 
 type WarningProps = {
-    warnings: Set<string>,
-    course: Course
-}
+    warnings: Set<string>;
+    course: Course;
+};
 
-function Warning({warnings, course}: WarningProps) {
-        return (
-            <>
-                {warnings.has("isAlsoOutside") && <div className="warningContainer outsideWarningContainer circle"
-                    title="Tento předmět je již ve tvém plánu.">
-                    <p className="warningSymbol">!</p>
-                </div>}
-                {warnings.has("unshownPredecessors") && <div className="warningContainer predsWarningContainer circle"
-                    title={"K tomuto předmětu v grafu chybí prerekvizity: " + course.unshownNeededPredecessors?.join(", ")}>
-                    <p className="warningSymbol">!</p>
-                </div>}
-            </>
-        );
+function Warning({ warnings, course }: WarningProps) {
+    return (
+        <>
+            {warnings.has('isAlsoOutside') && (
+                <div className="warningContainer outsideWarningContainer circle">
+                    <Tippy placement="bottom" content={'Tento předmět je již ve tvém plánu.'}>
+                        <p className="warningSymbol">!</p>
+                    </Tippy>
+                </div>
+            )}
+            {warnings.has('unshownPredecessors') && (
+                <div className="warningContainer predsWarningContainer circle">
+                    <Tippy
+                        placement="bottom"
+                        content={
+                            'K tomuto předmětu v grafu chybí prerekvizity: ' +
+                            course.unshownNeededPredecessors?.join(', ')
+                        }
+                    >
+                        <p className="warningSymbol">!</p>
+                    </Tippy>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default Warning;
