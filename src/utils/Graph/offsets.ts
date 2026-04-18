e1094861d2296da02a524b193325997e95495407
Leoš Hampl <llleosek@seznam.cz>
2026-04-18 14:21:01 +0200

    refactor: prettify

--- src/utils/Graph/offsets.ts
+++ src/utils/Graph/offsets.ts
@@ -1,14 +1,18 @@
 import { ensureOffset } from '@utils/Graph/dataUtils.js';
-import { EdgeOffsets, Details, Edge, OrderSubject, CodeToCoordinates, RealCoordinates, RealPositions, Spec } from '@/types/subjects';
+import { EdgeOffsets, Details, Edge, OrderSubject, CodeToPosition } from '@/types/subjects';
 import { Layout } from '@/consts/VisualisationParameters';
 
 const OFFSET_STEP = 12;
 
-
-export function getOffsets(details: Details, pos: RealPositions, plan: Record<string, Array<OrderSubject>>, codesToSem: Record<string, number>) {
+export function getOffsets(
+    details: Details,
+    pos: CodeToPosition,
+    plan: Record<string, Array<OrderSubject>>,
+    codesToSem: Record<string, number>
+) {
     const edgeXOffsets = {};
     const edgeYOffsets = {};
-    fillEdgeYOffsets(edgeYOffsets, details, plan, pos)
+    fillEdgeYOffsets(edgeYOffsets, details, plan, pos);
     fillEdgeXOffsets(edgeXOffsets, details, plan, pos, codesToSem);
     return [edgeXOffsets, edgeYOffsets];
 }
@@ -86,41 +90,53 @@ function shouldReverseSemesterSubjects(
     return validPairs > 0 && totalDiff / validPairs > 0;
 }
 
-function getShouldReverse(pos: RealPositions, parentCode: string, successors: Edge[]) {
+function getShouldReverse(pos: CodeToPosition, parentCode: string, successors: Edge[]) {
     const parentY = pos[parentCode].y ?? 0;
     let totalDiff = 0;
     let validSuccs = 0;
 
-    successors.forEach(s => {
+    successors.forEach((s) => {
         if (pos[s.code].y !== undefined) {
-            totalDiff += (pos[s.code].y - parentY);
+            totalDiff += pos[s.code].y - parentY;
             validSuccs++;
         }
     });
 
-    return validSuccs > 0 && (totalDiff / validSuccs) > 0;
+    return validSuccs > 0 && totalDiff / validSuccs > 0;
 }
 
-
-export function fillEdgeYOffsets(edgeYOffsets: EdgeOffsets, newDetails: Details, plan: Record<string, Array<OrderSubject>>, pos: RealPositions) {
+export function fillEdgeYOffsets(
+    edgeYOffsets: EdgeOffsets,
+    newDetails: Details,
+    plan: Record<string, Array<OrderSubject>>,
+    pos: CodeToPosition
+) {
     const orGroupEndOffsets: Record<string, number> = {};
-    const successorInDegreeCounter: Record<string, number> = {};
     const pathTargetOffsets: Record<string, number> = {};
-    Object.values(plan).forEach(semester => {
-        
+    Object.values(plan).forEach((semester) => {
         sortByPositions(semester, pos).forEach((subj) => {
-            const parentCode = "code" in subj ? subj.code : subj.choice;
-            if (!newDetails[parentCode]) {return;}
+            const parentCode = 'code' in subj ? subj.code : subj.choice;
+            if (!newDetails[parentCode]) {
+                return;
+            }
             sortByPositions(newDetails[parentCode].successors, pos).forEach((succ, i) => {
-                assignOffsets(parentCode, succ, newDetails, edgeYOffsets, orGroupEndOffsets, successorInDegreeCounter, i, pathTargetOffsets);
-            })
-        })
-    }
-    )
+                assignOffsets(
+                    parentCode,
+                    succ,
+                    newDetails,
+                    edgeYOffsets,
+                    orGroupEndOffsets,
+                    i,
+                    pathTargetOffsets,
+                    pos
+                );
+            });
+        });
+    });
 }
 
 function findRealSuccessor(currentCode: string, details: Details): string {
-    if (currentCode.includes("HELPER")) {
+    if (currentCode.includes('HELPER')) {
         const succs = details[currentCode]?.successors;
         if (succs && succs.length > 0) {
             return findRealSuccessor(succs[0].code, details);
@@ -129,52 +145,78 @@ function findRealSuccessor(currentCode: string, details: Details): string {
     return currentCode;
 }
 
-
-function sortByPositions<T extends { code: string } | { choice: string }>(array: Array<T>, pos: RealPositions): Array<T> {
+function sortByPositions<T extends { code: string } | { choice: string }>(
+    array: Array<T>,
+    pos: CodeToPosition
+): Array<T> {
     return [...array].sort((a, b) => {
-        const codeA = "code" in a ? a.code : a.choice;
-        const codeB = "code" in b ? b.code : b.choice;
+        const codeA = 'code' in a ? a.code : a.choice;
+        const codeB = 'code' in b ? b.code : b.choice;
         const yA = pos[codeA]?.y ?? 0;
         const yB = pos[codeB]?.y ?? 0;
         return yA - yB;
     });
 }
 
-
-function assignOffsets(parentCode: string, successorInfo: Edge, 
-                       oldDetails: Details, edgeYOffsets: Record<string, number>,
-                       orGroupEndOffsets: Record<string, number>, 
-                       successorInDegreeCounter: Record<string, number>, i: number, pathTargetOffsets: Record<string, number>) {
+function assignOffsets(
+    parentCode: string,
+    successorInfo: Edge,
+    oldDetails: Details,
+    edgeYOffsets: Record<string, number>,
+    orGroupEndOffsets: Record<string, number>,
+    i: number,
+    pathTargetOffsets: Record<string, number>,
+    pos: CodeToPosition
+) {
     const realSuccessorCode = findRealSuccessor(successorInfo.code, oldDetails);
-    const isParentHelper = parentCode.startsWith("HELPER");
+    const isParentHelper = parentCode.startsWith('HELPER');
 
     const pathKey = getPathCode(parentCode, realSuccessorCode, isParentHelper);
-    
-    const myGroup = successorInfo.groups?.find(g => g.includes(parentCode));
-    const definedSubject = myGroup
-        ?.find((subj) => pathTargetOffsets[getPathCode(subj, realSuccessorCode, subj.startsWith("HELPER"))] != undefined);
 
-    if (pathTargetOffsets[pathKey] == undefined) {
-        let newOffset: number;
-        
-        if (definedSubject) { // some subject from the OR-group is already defined
-            newOffset = pathTargetOffsets[getPathCode(definedSubject, realSuccessorCode, definedSubject.startsWith("HELPER"))] ;
-        } else {
-            newOffset = getEndOffset(realSuccessorCode, oldDetails, successorInDegreeCounter);
-        }
+    const myGroup = successorInfo.groups?.find((g) => g.includes(parentCode));
+    const definedSubject = myGroup?.find(
+        (subj) =>
+            pathTargetOffsets[getPathCode(subj, realSuccessorCode, subj.startsWith('HELPER'))] !=
+            undefined
+    );
 
-        if (myGroup) {
+    let endOffset: number;
+
+    if (myGroup) {
+        if (pathTargetOffsets[pathKey] == undefined) {
+            const newOffset = definedSubject
+                ? pathTargetOffsets[
+                      getPathCode(
+                          definedSubject,
+                          realSuccessorCode,
+                          definedSubject.startsWith('HELPER')
+                      )
+                  ]
+                : getEndOffset(parentCode, realSuccessorCode, oldDetails, pos);
+
             myGroup.forEach((memberCode) => {
-                const memberPathId = getPathCode(memberCode, realSuccessorCode, 
-                                                 memberCode.startsWith("HELPER"));
+                const memberPathId = getPathCode(
+                    memberCode,
+                    realSuccessorCode,
+                    memberCode.startsWith('HELPER')
+                );
                 pathTargetOffsets[memberPathId] = newOffset;
             });
+        }
+        endOffset = pathTargetOffsets[pathKey];
+    } else {
+        if (isParentHelper) {
+            const originalCode = parentCode.replace(/^HELPER_/, '').split('_')[0];
+            const originalPathKey = `${originalCode}_${realSuccessorCode}`;
+            endOffset =
+                pathTargetOffsets[originalPathKey] ??
+                getEndOffset(originalCode, realSuccessorCode, oldDetails, pos);
+            pathTargetOffsets[pathKey] = endOffset;
         } else {
-            pathTargetOffsets[pathKey] = newOffset;
+            endOffset = getEndOffset(parentCode, realSuccessorCode, oldDetails, pos);
+            pathTargetOffsets[pathKey] = endOffset;
         }
     }
-
-    const endOffset = pathTargetOffsets[pathKey];
 
     let startOffset = (i - (oldDetails[parentCode].successors.length - 1) / 2) * OFFSET_STEP;
 
@@ -182,20 +224,32 @@ function assignOffsets(parentCode: string, successorInfo: Edge,
     const finalEndOffset = endOffset;
 
     ensureOffset(edgeYOffsets, `${parentCode}-${successorInfo.code}-start`, finalStartOffset);
-    resolveEndOffset(edgeYOffsets, orGroupEndOffsets, parentCode,
-                     successorInfo.code, successorInfo.groups, finalEndOffset, successorInfo);
+    resolveEndOffset(
+        edgeYOffsets,
+        orGroupEndOffsets,
+        parentCode,
+        successorInfo.code,
+        successorInfo.groups,
+        finalEndOffset,
+        successorInfo
+    );
 }
 
 function getPathCode(startCode: string, endCode: string, isParentHelper: boolean) {
-    return (isParentHelper 
-        ? startCode.replace(/^HELPER_/, "").replace(/_\d+$/, "") 
-        : `${startCode}_${endCode}`);
+    return isParentHelper
+        ? startCode.replace(/^HELPER_/, '').replace(/_\d+$/, '')
+        : `${startCode}_${endCode}`;
 }
 
-
-function resolveEndOffset(edgeYOffsets: EdgeOffsets, orGroupEndOffsets: Record<string, number>,
-                          parentCode: string, succCode: string, groups: string[][],
-                          offset: number, successorInfo: Edge) : void {
+function resolveEndOffset(
+    edgeYOffsets: EdgeOffsets,
+    orGroupEndOffsets: Record<string, number>,
+    parentCode: string,
+    succCode: string,
+    groups: string[][],
+    offset: number,
+    successorInfo: Edge
+): void {
     if (!groups || groups.length == 0) {
         ensureOffset(edgeYOffsets, `${parentCode}-${succCode}-end`, offset);
         return;
@@ -203,7 +257,11 @@ function resolveEndOffset(edgeYOffsets: EdgeOffsets, orGroupEndOffsets: Record<s
     if (orGroupEndOffsets[`${parentCode}-${succCode}`] === undefined) {
         fillOrGroupOffsets(orGroupEndOffsets, successorInfo, offset);
     }
-    ensureOffset(edgeYOffsets, `${parentCode}-${succCode}-end`, orGroupEndOffsets[`${parentCode}-${succCode}`]);
+    ensureOffset(
+        edgeYOffsets,
+        `${parentCode}-${succCode}-end`,
+        orGroupEndOffsets[`${parentCode}-${succCode}`]
+    );
 }
 
 
@@ -218,28 +276,40 @@ function getEndOffset(succCode: string, oldDetails: Details, successorInDegreeCo
 }
 
 
-function getNumberOfSuccsBySemester(orderData: Record<string, Array<OrderSubject>>, infoData: Details, codesToSem: Record<string, number>) : Array<number> {
+function getNumberOfSuccsBySemester(
+    orderData: Record<string, Array<OrderSubject>>,
+    infoData: Details,
+    codesToSem: Record<string, number>
+): Array<number> {
     const numberOfSuccsBySemester = Array(Object.keys(orderData).length).fill(0);
     Object.entries(infoData).forEach(([code, course]) => {
         course.successors.forEach((successor) => {
-            if (!infoData[successor.code] || codesToSem[successor.code] == null) {return;}
+            if (!infoData[successor.code] || codesToSem[successor.code] == null) {
+                return;
+            }
             numberOfSuccsBySemester[Number(codesToSem[code]) - 1] += 1;
         });
     });
     return numberOfSuccsBySemester;
 }
 
-
-export function fillOrGroupOffsets(orGroupEndOffsets: Record<string, number>, edge: Edge, offset: number) : void {
+export function fillOrGroupOffsets(
+    orGroupEndOffsets: Record<string, number>,
+    edge: Edge,
+    offset: number
+): void {
     edge.groups.forEach((group) => {
         group.forEach((codeInGroup) => {
             orGroupEndOffsets[`${codeInGroup}-${edge.code}`] = offset;
-        })
-    })
+        });
+    });
 }
 
-
-export function getYOffsetForOrGroup(edgeYOffsets: EdgeOffsets, group: Array<string>, succCode: string) : number | undefined {
+export function getYOffsetForOrGroup(
+    edgeYOffsets: EdgeOffsets,
+    group: Array<string>,
+    succCode: string
+): number | undefined {
     let i = 0;
     while (i < group.length) {
         if (`${group[i]}-${succCode}-end` in edgeYOffsets) {
@@ -247,5 +317,5 @@ export function getYOffsetForOrGroup(edgeYOffsets: EdgeOffsets, group: Array<str
         }
         i++;
     }
-    console.warn("No offset found for OR group", group, "to", succCode);
+    console.warn('No offset found for OR group', group, 'to', succCode);
 }
