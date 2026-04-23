a326e144c5636c1d664bf7ccdca5fa8082432c5b
Leoš Hampl <llleosek@seznam.cz>
2026-04-23 17:49:48 +0200

    refactor: prettify

--- src/components/SideBar/RangeScaler.tsx
+++ src/components/SideBar/RangeScaler.tsx
@@ -1,23 +1,25 @@
-import Tippy from "@tippyjs/react";
-import "./styles.css"
-import { ZoomScale } from "@/consts/VisualisationParameters";
+import Tippy from '@tippyjs/react';
+import './styles.css';
+import { ZoomScale } from '@/consts/VisualisationParameters';
 
 type RangeScalerProps = {
-    scale: number,
-    setScale: React.Dispatch<React.SetStateAction<number>>
-}
+    scale: number;
+    setScale: React.Dispatch<React.SetStateAction<number>>;
+};
 
-
-function RangeScaler({scale, setScale} : RangeScalerProps) {
+function RangeScaler({ scale, setScale }: RangeScalerProps) {
     return (
-        <div className='rangeScaler desktopOnly'>
+        <div className="rangeScaler desktopOnly">
             <label>Velikost</label>
             <Tippy placement="bottom-end" content="Velikost lze měnit také pomocí Ctrl + scroll">
-                <div className="helpIcon" style={{ width: "12px", height: "12px", marginLeft: "2px", fontSize: "10px" }}>
+                <div
+                    className="helpIcon"
+                    style={{ width: '12px', height: '12px', marginLeft: '2px', fontSize: '10px' }}
+                >
                     ?
                 </div>
             </Tippy>
-            <input    
+            <input
                 type="range"
                 min={ZoomScale.logMin}
                 max={ZoomScale.logMax}
@@ -25,7 +27,7 @@ function RangeScaler({scale, setScale} : RangeScalerProps) {
                 value={Math.log(scale)}
                 onChange={(e) => setScale(Math.exp(Number(e.target.value)))}
             ></input>
-            </div>
+        </div>
     );
 }
 
