f3c6fb5153cfffa357a935383e7abaefc4cfdd8b
Leoš Hampl <llleosek@seznam.cz>
2026-04-23 16:43:43 +0200

    refactor: prettify

--- src/components/SideBar/CheckboxField.tsx
+++ src/components/SideBar/CheckboxField.tsx
@@ -1,18 +1,36 @@
 type CheckBoxFieldProps = {
-    children: React.ReactNode,
-    checked: boolean,
+    children: React.ReactNode;
+    checked: boolean;
     onChange: () => void;
     onMouseEnter?: () => void;
     onMouseLeave?: () => void;
     fieldType: string;
-}
+};
 
-function CheckBoxField({children, checked, onChange, onMouseEnter, onMouseLeave, fieldType} : CheckBoxFieldProps) {
+function CheckBoxField({
+    children,
+    checked,
+    onChange,
+    onMouseEnter,
+    onMouseLeave,
+    fieldType
+}: CheckBoxFieldProps) {
     return (
-        <div className={`field advancedField advanced${fieldType}Field`} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onChange}>
+        <div
+            className={`field advancedField advanced${fieldType}Field`}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            onClick={onChange}
+        >
             <label className="preferenceFieldLabel">{children}</label>
-            <input id="check" type="checkbox" checked={checked} style={{ marginTop: "5px", alignSelf: "center" }}/>
+            <input
+                id="check"
+                type="checkbox"
+                checked={checked}
+                readOnly
+                style={{ marginTop: '5px', alignSelf: 'center' }}
+            />
         </div>
-    )
+    );
 }
-export default CheckBoxField
+export default CheckBoxField;
