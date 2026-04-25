f58e85bbe4dcd0c13563dec1960641775dee5f51
Leoš Hampl <llleosek@seznam.cz>
2026-04-25 20:23:37 +0200

    refactor: add github page config and edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url))
+    plugins: [react()],
+    base: '/bioinformatics-visualiser/',
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+            '@components': fileURLToPath(new URL('./src/components', import.meta.url))
+        }
     }
-  }
 });
