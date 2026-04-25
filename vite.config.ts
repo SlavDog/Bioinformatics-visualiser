b5e2247e5985e96d02a2974a88d74b283fce7e3f
Leoš Hampl <llleosek@seznam.cz>
2026-04-25 21:01:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/bioinformatics-visualiser/',
+    base: './',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
