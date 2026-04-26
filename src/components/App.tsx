73830f019c37c6aee120121e6a3a6ded2dde8d06
Leoš Hampl <llleosek@seznam.cz>
2026-04-26 12:50:28 +0200

    refactor: prettify App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,15 @@
-import { Layout } from '@/consts/VisualisationParameters';
+import { Layout } from '@/consts/visualisationParameters';
 import DragScrollBox from '@components/Visualisation/DragScrollBox.jsx';
 import { SubjectDataProvider } from '@components/providers/dataProvider';
 
 function App() {
-  return (
-    <div style={{ "--sidebar-width": `${Layout.sidebarWidth}px` } as React.CSSProperties}>
-      <SubjectDataProvider>
-        <DragScrollBox/>
-      </SubjectDataProvider>
-    </div>
-  );
+    return (
+        <div style={{ '--sidebar-width': `${Layout.sidebarWidth}px` } as React.CSSProperties}>
+            <SubjectDataProvider>
+                <DragScrollBox />
+            </SubjectDataProvider>
+        </div>
+    );
 }
 
-export default App
+export default App;
