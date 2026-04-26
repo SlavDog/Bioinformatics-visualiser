c8e68f9cbc5606e0327f177b794d22025019fbf7
Leoš Hampl <llleosek@seznam.cz>
2026-04-26 12:54:15 +0200

    refactor: add emptyNode to consts

---
+++ src/consts/emptyNode.tsx
@@ -0,0 +1,13 @@
+export const emptyNode = {
+    name: '',
+    faculty: '',
+    successors: [],
+    predecessors: [],
+    language: '',
+    completion: '',
+    has_successors: true,
+    has_parent: true,
+    credits: 0,
+    link: '',
+    type: ''
+};
